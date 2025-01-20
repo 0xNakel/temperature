b0c1a15d9a4b49818c9f9cb8c0865bbb99c5e6fe
@sshdavid <195252584+sshdavid@users.noreply.github.com>
2025-01-19 20:18:23 -0800

    added very basic conerstion to celsius

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,31 @@
+use std::io;
+use std::io::Write;
+
+
 fn main() {
-    println!("Temperature Converter");
-    println!("This is a work in progress.");
+    println!("Temperature Converter!");
+
+    print!("Enter the temperature in Fahrenheit: ");
+    io::stdout().flush().unwrap();
+
+    let mut faren = String::new();
+
+    io::stdin()
+        .read_line(&mut faren)
+        .expect("Could not read line");
+
+    let faren:f64 = faren.trim()
+        .parse()
+        .expect("Only numbers are accepted!");
+
+    
+    let cel:f64 = (({faren} - 32.0) * 5.0) / 9.0;
+
+    println!("The temperature in Celcius is: {cel}")
+
+
+
+
+
+
 }
