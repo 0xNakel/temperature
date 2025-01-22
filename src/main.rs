2d67c7f27a707b2a7f896a103bdeffeda1708b93
David <195252584+sshdavid@users.noreply.github.com>
2025-01-21 16:56:09 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,39 @@
 use std::io;
 use std::io::Write;
 
+enum TempScale {
+    C,
+    F,
+    K,
+}
+
+
 fn main () {
     println! ("Temperature Converter v 0.1.0");
     
-    let mut input = String::new();
+    print! ("Please enter if you want ° F or ° C: ");
+    io::stdout().flush();
+    
+    let mut which_scale = String::new();
+    
+    io::stdin().read_line(&mut which_scale)
+            .expect("Could not read line.");
+
     
     print! ("Enter temperature in Fahrenheit: ");
     io::stdout().flush();
     
-    io::stdin().read_line(&mut input)
+    let mut fa_deg = String::new();
+    io::stdin().read_line(&mut fa_deg)
             .expect("Could not read line.");
     
-    let temp: f32 = match input.trim().parse() {
+    let fa_deg: f32 = match fa_deg.trim().parse() {
         Ok(num) => num,
         Err(_) => panic!("Not a number! Try again.")
     };
     
-    let celsius  = ((temp - 32.0) / (9.0/5.0));
+    let cel_deg  = (fa_deg - 32.0) / (9.0/5.0);
     
-    println!("{celsius}");
+    println!("{cel_deg}");
     
 }
