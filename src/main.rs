f3eb77dfcb8572508e25d7fc6d9ebca650d04c98
@sshdavid <195252584+sshdavid@users.noreply.github.com>
2025-01-23 21:53:02 -0800

    Revamped some code and added choice func

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,78 @@
-use std::io::{self, Write};
+use std::io::{self, BufRead, Write};
 
+enum Options {
+    F,
+    C,
+    K,
+    Q,
+}
+#[derive(Debug)]
+struct InputError;
 
-fn main () {
-    println! ("Temperature Converter v 0.1.0");
+fn main() {
+    println!("Temperature Converter v 0.1.0");
 
-    print! ("Select starting unit [F]ahrenheit [C]elsius [K]elvin or [Q]uit: ");
-    io::stdout().flush();
-
-    fn prompt_choice() -> Result <String, io::Error> {
-        let mut sel_choice = String::new();
-        io::stdin().read_line(&mut sel_choice)?;
-        Ok(sel_choice)
+    impl Options {
+        fn prompt_choice(c: &str) -> Result<Options, InputError> {
+            match c {
+                "F" | "f" => Ok(Options::F),
+                "C" | "c" => Ok(Options::C),
+                "K" | "k" => Ok(Options::K),
+                "Q" | "q" => Ok(Options::Q),
+                _ => Err(InputError),
+            }
+        }
     }
 
-    
-
-
-    match Result {
-        1 => println!("Fahrenheit chosen"),
-        2 => println!("Celsius chosen"),
-        3 => println!("Not yet implemented"),
-        4 => return,
-        _ => println! ("what")
+    fn enum_match(scale: Options) {
+        match scale {
+            Options::F => unimplemented!(),
+            Options::C => unimplemented!(),
+            Options::K => unimplemented!(),
+            Options::Q => std::process::exit(0),
+        }
     }
-    
-    print! ("Enter temperature in Fahrenheit: ");
+
+    print!("Select starting unit [F]ahrenheit [C]elsius [K]elvin or [Q]uit: ");
     io::stdout().flush();
-    
+    let mut input = String::new();
+    std::io::BufReader::new(std::io::stdin())
+        .read_line(&mut input)
+        .unwrap();
+    let choice = Options::prompt_choice(input.trim()).unwrap();
+    enum_match(choice);
+
+    print!("Enter temperature in Fahrenheit: ");
+    io::stdout().flush();
+
     let mut fa_deg = String::new();
-    io::stdin().read_line(&mut fa_deg)
-            .expect("Could not read line.");
-    
+    io::stdin()
+        .read_line(&mut fa_deg)
+        .expect("Could not read line.");
+
     let fa_deg: f32 = match fa_deg.trim().parse() {
         Ok(num) => num,
-        Err(e) => panic!("{e}")
+        Err(e) => panic!("{e}"),
     };
-    
-    let cel_deg  = (fa_deg - 32.0) / (9.0/5.0);
-    
+
+    let cel_deg = (fa_deg - 32.0) / (9.0 / 5.0);
+
     println!("{cel_deg} °C");
-    
-    
-    print! ("Enter temperature in Celsius: ");
+
+    print!("Enter temperature in Celsius: ");
     io::stdout().flush();
-    
+
     let mut cel_deg = String::new();
-    io::stdin().read_line(&mut cel_deg)
-            .expect("Could not read line.");
-    
+    io::stdin()
+        .read_line(&mut cel_deg)
+        .expect("Could not read line.");
+
     let cel_deg: f32 = match cel_deg.trim().parse() {
         Ok(num) => num,
-        Err(_) => panic!("Not a number! Try again.")
+        Err(_) => panic!("Not a number! Try again."),
     };
-    
-    let fax_deg  = cel_deg * (9.0/5.0) + 32.0;
-    
+
+    let fax_deg = cel_deg * (9.0 / 5.0) + 32.0;
+
     println!("{fax_deg} °F")
-    
 }
