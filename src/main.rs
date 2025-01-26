95d91bc0121d77702bffff20ac852ddf861e808a
@sshdavid <195252584+sshdavid@users.noreply.github.com>
2025-01-25 21:33:28 -0800

    woo, actually working now

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
-use std::io::{self, BufRead, Write};
+use std::io::{self, BufRead};
 use std::num::ParseFloatError;
+use std::process::exit;
 
 fn main() {
     println!("Temperature Converter v 0.1.0");
 
-    print!("Select starting scale  [F]ahrenheit [C]elsius [K]elvin or [Q]uit: ");
-    io::stdout().flush();
+    fn get_input() -> String {
+        println!("Select starting scale:  [F]ahrenheit [C]elsius [K]elvin or [Q]uit ");
 
-    let mut input = String::new();
-    std::io::BufReader::new(std::io::stdin())
-        .read_line(&mut input)
-        .unwrap();
+        let mut input = String::new();
+        std::io::BufReader::new(std::io::stdin())
+            .read_line(&mut input)
+            .expect("Input not found!");
+        input
+    }
+
+    let selection = get_input();
+
+    match selection.trim() {
+        "F" | "f" => match f_to_c() {
+            Ok(c_deg) => println!("Your temperature in Celsius is: {c_deg}"),
+            Err(err) => panic!("{err}"),
+        },
+        "C" | "c" => match c_to_f() {
+            Ok(f_deg) => println!("Your temperature in Fahrenheit is: {f_deg}"),
+            Err(err) => panic!("{err}"),
+        },
+        "Q" | "q" => exit(0),
+        _ => println!("Option not recognized"),
+    }
 
     fn f_to_c() -> Result<f32, ParseFloatError> {
-        print!("Enter temperature in Fahrenheit: ");
-        io::stdout().flush();
+        println!("Enter temperature in Fahrenheit: ");
 
         let mut f_deg = String::new();
         io::stdin().read_line(&mut f_deg).unwrap();
@@ -27,8 +44,7 @@ fn main() {
     }
 
     fn c_to_f() -> Result<f32, ParseFloatError> {
-        print!("Enter temperature in Celsius: ");
-        io::stdout().flush();
+        println!("Enter temperature in Celsius: ");
 
         let mut c_deg = String::new();
         io::stdin().read_line(&mut c_deg).unwrap();
